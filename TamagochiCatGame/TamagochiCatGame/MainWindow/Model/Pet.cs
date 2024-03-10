f3a3629252b4bee351164c3df901ef46443623a0
Darantar <Darantar@DESKTOP-EK9VSKR>
2024-03-11 00:05:34 +0700

    Добавлен уровень модели, добавлены механники. Добавлено окно ввода имени.

--- TamagochiCatGame/TamagochiCatGame/MainWindow/Model/Pet.cs
+++ TamagochiCatGame/TamagochiCatGame/MainWindow/Model/Pet.cs
@@ -2,12 +2,243 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TamagochiCatGame
 {
     internal class Pet
     {
+        #region Свойства питомца
 
+        /// <summary>
+        /// Имя питомца.
+        /// </summary>
+        public string Name {  get; set; }
+
+        /// <summary>
+        /// Возраст питомца.
+        /// </summary>
+        public string Age { get; set; }
+
+        /// <summary>
+        /// Отображает текущее здоровье питомца.
+        /// </summary>
+        public uint Health { get; set; }
+
+        /// <summary>
+        /// Отображает текущее здоровье питомца.
+        /// </summary>
+        public uint Hunger { get; set; }
+
+        /// <summary>
+        /// Отображает текущее здоровье питомца.
+        /// </summary>
+        public uint Stamina { get; set; }
+
+        /// <summary>
+        /// Отображает текущее настроение питомца.
+        /// </summary>
+        public uint Mood { get; set; }
+
+        /// <summary>
+        /// Отображает жив ли ваш питомец.
+        /// </summary>
+        public bool Alive { get; set; }
+        #endregion
+
+        #region Методы питомца
+
+
+        /// <summary>
+        /// Метод таймерного события снижения характеристик.
+        /// </summary>
+        public void DecimalEvent()
+        {
+            if (Name != null && Alive) 
+            {
+                if (Health == 0)
+                {
+                    DiyEvent();
+                }
+                else
+                {
+                    if (Hunger >= 10)
+                    {
+                        Hunger = Hunger - 10;
+                    }
+                    else
+                    {
+                        if (Hunger == 0)
+                        {
+                            Health = Health - 10;
+                            System.Windows.MessageBox.Show("Ваш питомец голодает.");
+                        }
+                    }
+
+                    if (Mood >= 10)
+                    {
+                        Mood = Mood - 10;
+                    }
+                    else
+                    {
+                        if (Mood == 0)
+                        {
+                            Stamina = Stamina / 2;
+                            System.Windows.MessageBox.Show("Ваш питомец в депрессии.");
+                        }
+                        else
+                        {
+                            Mood = 0;
+                        }
+                    }
+
+                    return;
+                }
+                
+            }
+            else { return; }
+
+        }
+
+        /// <summary>
+        /// Метод первоначльного создания питомца.
+        /// </summary>
+        public void Create(string name)
+        {
+            Name = name;
+            Alive = true;
+            Health = 100;
+            Hunger = 80;
+            Mood = 80;
+            Stamina = 80;
+        }
+
+        /// <summary>
+        /// Метод кормления питомца.
+        /// </summary>
+        public void Feed()
+        {
+            if (Hunger < 100 && Health > 0) 
+            {
+                Hunger = Hunger + 10;
+                Mood = Mood + 5;
+            }
+            else
+            {   
+                if (Health == 0) 
+                {
+                    System.Windows.MessageBox.Show("У вас нет питомца.");
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Ваш питомец не голоден.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод игры с питомцем.
+        /// </summary>
+        public void Play()
+        {
+            if (Mood < 100 && Stamina >= 10 && Health > 0)
+            {
+                Mood = Mood + 10;
+                Stamina = Stamina - 10;
+            }
+            else
+            {
+                if (Health == 0)
+                {
+                    System.Windows.MessageBox.Show("У вас нет питомца.");
+                }
+                else
+                {
+                    if (Mood == 100)
+                    {
+                        System.Windows.MessageBox.Show("Ваш питомец не хочет играть.");
+                    }
+                    if (Stamina <= 10)
+                    {
+                        System.Windows.MessageBox.Show("У вашего питомца нет сил играть.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод укачивания питомца.
+        /// </summary>
+        public void Lul()
+        {
+            if (Stamina < 100 && Hunger >= 10 && Health > 0)
+            {
+                Stamina = 100;
+                Hunger = Hunger - 10;
+            }
+            else
+            {
+                if (Health == 0)
+                {
+                    System.Windows.MessageBox.Show("У вас нет питомца.");
+                }
+                else
+                {
+                    if (Hunger <= 10)
+                    {
+                        System.Windows.MessageBox.Show("Ваш питомец не может уснуть от голода.");
+                    }
+                    if (Stamina == 100)
+                    {
+                        System.Windows.MessageBox.Show("Ваш питомец абсолютно бодр.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод лечения питомца.
+        /// </summary>
+        public void Treat()
+        {
+            if (Health < 100 && Stamina >= 10 && Health > 0)
+            {
+                Health = Health + 10;
+                Stamina = Stamina - 10;
+            }
+            else
+            {
+                if (Health == 0)
+                {
+                    System.Windows.MessageBox.Show("У вас нет питомца.");
+                }
+                else
+                {
+                    if (Stamina <= 10)
+                    {
+                        System.Windows.MessageBox.Show("Ваш слишком слаб, чтобы принять лечение.");
+                    }
+                    if (Health == 100)
+                    {
+                        System.Windows.MessageBox.Show("Ваш питомец абсолютно здоров.");
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Метод вызова события смерти питомца по достижению ее условий.
+        /// </summary>
+        public void DiyEvent()
+        {
+            Alive = false;
+            Stamina = 0;
+            Hunger = 0;
+            Mood = 0;
+            System.Windows.MessageBox.Show("Ваш питомец мертв. Вы никудышный хозяин.");
+        }
+        #endregion
     }
 }
--- TamagochiCatGame/TamagochiCatGame/MainWindow/ViewModel/MainWindowl.xaml.cs
+++ TamagochiCatGame/TamagochiCatGame/MainWindow/ViewModel/MainWindowl.xaml.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,17 +13,168 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TamagochiCatGame.PetCreateWindow.View;
+using static System.Collections.Specialized.BitVector32;
+
+
+
 
 namespace TamagochiCatGame
 {
+
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Обьект текущего питомца пользователя.
+        /// </summary>
+        Pet CurrentPet = new  Pet();
+
+        /// <summary>
+        /// Таймер события уменьшения голода и настроения со временем
+        /// </summary>
+        public static System.Timers.Timer DecimalTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+            PetInishalyze();
+            PetDecimalingTimer();
+            PetViewModalSynq();
         }
+
+
+
+
+        /// <summary>
+        /// Создание или загрузка из файла питомца со стартавыми/сохраненными свойствами
+        /// </summary>
+        public void PetInishalyze()
+        {
+            //Вариант запуска не из сохранения
+            CurrentPet.Name = string.Empty;
+            CurrentPet.Age = "0 лет, 0 дней, 0 часов";
+            CurrentPet.Stamina = 0;
+            CurrentPet.Health = 0;
+            CurrentPet.Hunger = 0;
+            CurrentPet.Mood = 0;
+            
+
+            //Синхронизация с View
+            Health_Bar.Value = CurrentPet.Health;
+            Health_Status_TextBlock.Text = CurrentPet.Health.ToString();
+            Hunger_Bar.Value = CurrentPet.Hunger;
+            Hunger_Status_TextBlock.Text = CurrentPet.Hunger.ToString();
+            Mood_Bar.Value = CurrentPet.Mood;
+            Mood_Status_TextBlock.Text = CurrentPet.Mood.ToString();
+            Stamina_Bar.Value = CurrentPet.Stamina;
+            Stamina_Status_TextBlock.Text =CurrentPet.Stamina.ToString();
+
+        }
+
+        /// <summary>
+        /// Инициализация события уменьшения голода и настроения со временем
+        /// </summary>
+        public void PetDecimalingTimer()
+        {
+            DecimalTimer = new System.Timers.Timer(2000);
+            DecimalTimer.Elapsed += TimedDecimalEvent;
+            DecimalTimer.AutoReset = true;
+            DecimalTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Обработка события таймера
+        /// </summary>
+        public void TimedDecimalEvent(Object source, ElapsedEventArgs e)
+        {
+            CurrentPet.DecimalEvent();
+            
+        }
+
+        /// <summary>
+        /// Метод синхронизации MVVM для класса питомца. В случае необходимости развития проекта лучше заменять на INotifyPropertyChanged.
+        /// </summary>
+        public void PetViewModalSynq()
+        {
+            Health_Bar.Value = CurrentPet.Health;
+            Health_Status_TextBlock.Text = CurrentPet.Health.ToString();
+            Hunger_Bar.Value = CurrentPet.Hunger;
+            Hunger_Status_TextBlock.Text = CurrentPet.Hunger.ToString();
+            Mood_Bar.Value = CurrentPet.Mood;
+            Mood_Status_TextBlock.Text = CurrentPet.Mood.ToString();
+            Stamina_Bar.Value = CurrentPet.Stamina;
+            Stamina_Status_TextBlock.Text = CurrentPet.Stamina.ToString();
+            if (CurrentPet.Name == null || CurrentPet.Name == string.Empty)
+            {
+                Pet_Name_Button.Content = "Создать питомца";
+            }
+            else
+            {
+                Pet_Name_Button.Content = CurrentPet.Name;
+            }
+        }
+
+        public void Pet_Name_Button_Click(object sender, EventArgs e)
+        {
+            if (CurrentPet.Name == null || CurrentPet.Name == string.Empty)
+            {
+                PetCreateWindow.View.PetCreateWindow PetCreateWindow = new PetCreateWindow.View.PetCreateWindow();
+                
+
+                if (PetCreateWindow.ShowDialog() == true)
+                {
+                    CurrentPet.Create(PetCreateWindow.name);
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Похоже у вас уже есть питомец.");
+                }
+
+            }
+            PetViewModalSynq();
+        }
+
+        public void Feed_Pet_Button_Click(object sender, EventArgs e)
+        {
+            if(CurrentPet != null)
+            {
+                CurrentPet.Feed();
+            }
+            PetViewModalSynq();
+        }
+
+        public void Play_Pet_Button_Click(object sender, EventArgs e)
+        {
+            if (CurrentPet != null)
+            {
+                CurrentPet.Play();
+            }
+            PetViewModalSynq();
+        }
+        public void Lul_Pet_Button_Click(object sender, EventArgs e)
+        {
+            if (CurrentPet != null)
+            {
+                CurrentPet.Lul();
+            }
+            PetViewModalSynq();
+        }
+        public void Treat_Pet_Button_Click(object sender, EventArgs e)
+        {
+            if (CurrentPet != null)
+            {
+                CurrentPet.Treat();
+            }
+            PetViewModalSynq();
+        }
+
+
+
+
+
     }
 }
